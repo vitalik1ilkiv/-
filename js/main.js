c07203d738dd95591fffad35e937dd5a00a61a4c
vitalik1ilkiv <62646057+vitalik1ilkiv@users.noreply.github.com>
2021-02-20 18:50:55 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let tab = function(){
+	let tabNav = document.querySelectorAll('.tabs-nav__item'),
+		 tabContent = document.querySelectorAll('.tab'),
+		 tabName;
+	tabNav.forEach(item=>{
+		console.log(tabNav);
+		item.addEventListener('click', selectTabNav);
+	});
+
+	function selectTabNav(){
+		tabNav.forEach(item=> {
+			item.classList.remove('is-active');
+		});
+		this.classList.add('is-active');
+		tabName = this.getAttribute('data-tab-name');
+		selectTabContent(tabName);
+	}
+
+	function selectTabContent(tabName){
+		tabContent.forEach(item =>{
+			item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+		})
+	}
+};
+
+tab();
+
+$('pricing__item').mouseover(function(){
+$(this).addClass('hover');
+});
+$('pricing__item').mouseleave(function(){
+$(this).removeClass('hover');
+});
+
